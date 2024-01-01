bcfbd88739ea50c2c67b1e9a79a0f576b5429ae1
SociallyIneptWeeb <sociallyinept.csit@gmail.com>
2024-01-01 15:34:32 +0800

    feat: Add more thread data to db

--- db/data.sql
+++ db/data.sql
@@ -225,6 +225,18 @@ COPY public.comments (id, created_at, updated_at, content, user_id, thread_id) F
 20	2023-12-26 06:14:56.04265+00	2023-12-26 06:14:56.04265+00	Well I didn't knew about the voice actor but, I follow your emotion. What happened was tragedy both in anime and real life.	4	5
 21	2023-12-26 06:15:31.692589+00	2023-12-26 06:15:31.692589+00	It's a shame we lost such a great talent so early. My deepest regards to her family and friends and rest in peace.	2	5
 22	2023-12-26 06:16:17.668711+00	2023-12-26 06:16:17.668711+00	Agreed. Still sad about her passing, her music, especially in this movie was so good.	1	5
+23	2023-12-29 03:34:21.881845+00	2023-12-29 03:34:21.881845+00	Battle shounen have never been good at characterization.	2	6
+24	2023-12-29 03:34:43.756451+00	2023-12-29 03:34:43.756451+00	Chainsaw Man's real selling point, beneath the chainsaws and the action, is exactly the issue you're addressing. At least that's what it tries to do, whether it succeeds or not depends on you, everyone's taste is different. Some of it can already be seen in what was adapted so far, but CSM would have really benefitted from a 24 episodes first season like its competitors.	3	6
+25	2023-12-29 03:35:03.380222+00	2023-12-29 03:35:03.380222+00	I think it's just a matter of personal taste and how tolerant you are of certain characterizations over time. I mean, even when you look at older shounen series like DB, Saint Seiya, Yu Yu Hakusho, and Hunter x Hunter, a lot of the characterizations aren't too different from today's anime. Of course, if you grew up with those series, the characterizations may seem more original back then than what is out now. But from the standpoint of a modern shounen watcher, there may not be much of a difference.	4	6
+26	2023-12-29 03:35:27.773131+00	2023-12-29 03:35:27.773131+00	It's honestly all relative. I disagree with you on Demon Slayer; it's fast-paced during many of the battle segments, but in my opinion it does a fine job in between of exploring each character and building attachment to them.\n\nI'd say modern battle shonen are no worse at it than classic series. If anything, there are more attempts in modern series than there used to be to slow things down and take a character-driven approach. But again, two people could have entirely different opinions for the same series about this.	5	6
+27	2024-01-01 05:19:01.260292+00	2024-01-01 05:19:01.260292+00	Most 1's are from people who are overly biased idiots that try to mask the good qualities of a manga/anime just because they got mad at some of the flaws of it.	3	7
+28	2024-01-01 05:19:21.494313+00	2024-01-01 05:19:21.494313+00	I personally don't give a 1 to any anime apart from anime like "Mars of destruction". They are the lowest effort trash and worthless anime. Otherwise I don't see any reason to give a popular and well loved anime a 1 rating even if I hated it.	4	7
+29	2024-01-01 05:19:39.614754+00	2024-01-01 05:19:39.614754+00	I don't get offended by anime. For me, a 1 just means that it was criminally boring.	5	7
+30	2024-01-01 05:20:07.00344+00	2024-01-01 05:20:07.00344+00	I almost never rate anime one or ten in recent years, because I believe that 99% of such ratings are dictated by pure emotion, not reason.	1	7
+31	2024-01-01 07:20:55.106348+00	2024-01-01 07:20:55.106348+00	K-On!! 8.17	4	8
+32	2024-01-01 07:21:11.499914+00	2024-01-01 07:21:11.499914+00	the most correctly Hajime no Ippo	5	8
+33	2024-01-01 07:21:31.475568+00	2024-01-01 07:21:31.475568+00	even though I gave it a 10, with the amount of contrarians in the anime community, it's a testament to how good Steins Gate actually is that it's maintained a 9 rating for so many years.	3	8
+34	2024-01-01 07:21:50.228122+00	2024-01-01 07:21:50.228122+00	I would say Hunter x Hunter (2011) with 9,04 and 1.7mio ratings is the rating truly correctly.	2	8
 \.
 
 
@@ -238,6 +250,9 @@ COPY public.threads (id, created_at, updated_at, title, description, category_id
 3	2023-12-24 10:26:17.28436+00	2023-12-24 10:26:17.28436+00	Spy x Family Season 2 Episode 12 is OUT!	Did you guys enjoy the latest episode of Spy x Family S2???	1	2	2023-12-24 10:26:17.28436+00
 4	2023-12-25 07:38:22.513465+00	2023-12-25 07:38:22.513465+00	Cid from the Eminence in Shadow Love interest	I am 10 episodes in but I'm wondering if Cid is going to end up with a girl at the start I thought he was going to end up with Alexia but the more I watch I think he's just going to jump from girl to girl.	3	4	2023-12-25 07:38:22.513465+00
 5	2023-12-26 06:14:14.586228+00	2023-12-26 06:14:14.586228+00	Feel sorry for Yuna	Just rewatching this reminded me of Kanda Sayaka, the Japanese voice actor of Yuna, the anniversary of whose death has just passed a few days ago. When I saw Yuna in the movie, I can't help feeling sorry for both her and Kanda. It somehow made the movie a more emotional one.	1	5	2023-12-26 06:14:14.586228+00
+6	2023-12-29 03:33:45.957473+00	2023-12-29 03:33:45.957473+00	Modern battle shonen are bad at characterization	I just finished JJK season 2, despite, consisting of many high-stake cool fights that give edge-to-seat experience. I do not feel any emotional attachment to the characters and even their supposed final moments fail to bring any sort of emotion out of me.\n\nIt's not just female characters that are poorly treated but male characters as well. A recent look into the way these battle shonen like Demon slayer and Jujutsu kaisen. They are super-fast paced shows that are focused on providing instant gratification and fail to explore characters and inner world in a meaningful way.	2	1	2023-12-29 03:33:45.957473+00
+7	2024-01-01 05:18:40.954996+00	2024-01-01 05:18:40.954996+00	How often does a score of 1 mean that you were offended?	In other words, would an anime have to basically make you triggered in order for you to give it a score of 1?\n\nJust wanted to know a bit more about the thought process behind people's scores	3	2	2024-01-01 05:18:40.954996+00
+8	2024-01-01 07:20:36.555479+00	2024-01-01 07:20:36.555479+00	The most correctly rated anime	Which anime do you think is the most correctly rated?	1	1	2024-01-01 07:20:36.555479+00
 \.
 
 
@@ -265,14 +280,14 @@ SELECT pg_catalog.setval('public.categories_id_seq', 3, true);
 -- Name: comments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
 --
 
-SELECT pg_catalog.setval('public.comments_id_seq', 22, true);
+SELECT pg_catalog.setval('public.comments_id_seq', 34, true);
 
 
 --
 -- Name: threads_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
 --
 
-SELECT pg_catalog.setval('public.threads_id_seq', 5, true);
+SELECT pg_catalog.setval('public.threads_id_seq', 8, true);
 
 
 --
